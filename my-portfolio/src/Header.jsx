3aca93c071aac34b171e189c9c7a5e1547d34ce1
Umida <umidakhongaybullaeva@gmail.com>
2022-12-18 01:39:47 -0500

    pushing my portfolio

--- my-portfolio/src/Header.jsx
+++ my-portfolio/src/Header.jsx
@@ -1,7 +1,11 @@
+import Nav from './Nav'
 export default function () {
     return (
         <div className="header">
             <h1>Header</h1>
+            <div className="header-nav">
+                <Nav />
+            </div>
         </div>
     )
 }
